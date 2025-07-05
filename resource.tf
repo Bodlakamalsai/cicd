92a4119bfb7cbfc15fabd667d626a34737877aa5
Kamalsai Bodla <bodlakamalsai@gmail.com>
2025-07-05 22:44:10 +0530

    resource file

---
+++ resource.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "web-bucket-kamal"
+
+}
+
+resource "aws_instance" "terraform" {
+    ami = "ami-020cba7c55df1f615"
+    instance_type = "t2.micro"
+    key_name = "kamal"
+}
